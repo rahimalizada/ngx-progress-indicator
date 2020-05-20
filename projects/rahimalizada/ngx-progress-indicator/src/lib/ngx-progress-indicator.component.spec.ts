72ab6cd32e3862702bec9a56484e17fc77ef3eec
Rahim Alizada <rahim.alizada@gmail.com>
2020-05-20 15:49:05 +0400

    Added a library project

---
+++ projects/rahimalizada/ngx-progress-indicator/src/lib/ngx-progress-indicator.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NgxProgressIndicatorComponent } from './ngx-progress-indicator.component';
+
+describe('NgxProgressIndicatorComponent', () => {
+  let component: NgxProgressIndicatorComponent;
+  let fixture: ComponentFixture<NgxProgressIndicatorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NgxProgressIndicatorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NgxProgressIndicatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/rahimalizada/ngx-progress-indicator/src/lib/ngx-progress-indicator.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxProgressIndicatorComponent } from './ngx-progress-indicator.component';
+
+
+
+@NgModule({
+  declarations: [NgxProgressIndicatorComponent],
+  imports: [
+  ],
+  exports: [NgxProgressIndicatorComponent]
+})
+export class NgxProgressIndicatorModule { }
